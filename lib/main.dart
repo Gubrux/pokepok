d6b3d57f03bf6b231c0014dc9ea354d5e771bf9e
gabriel <gabrielcentur@gmail.com>
2025-07-06 18:17:45 -0300

    Refactor main function to initialize SharedPreferences and integrate with Riverpod for state management. Update import paths for better organization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pokepok/home_view.dart';
+import 'package:pokepok/views/home_view.dart';
+import 'package:pokepok/providers/shared_preferences_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const ProviderScope(child: Pokepok()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ],
+      child: const Pokepok(),
+    ),
+  );
 }
 
 class Pokepok extends StatelessWidget {
