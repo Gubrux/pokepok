afebd2cdd39881816d1750db809d795f13186fbb
gabriel <gabrielcentur@gmail.com>
2025-07-06 18:17:10 -0300

    Add HomeView to implement pagination for Pokémon display. Integrate PageView for vertical scrolling and fetch more Pokémon as the user approaches the end of the list, enhancing user experience with Riverpod state management.

---
+++ lib/views/home_view.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokepok/providers/pokemon_provider.dart';
+import 'package:pokepok/views/pokemon_main_view.dart';
+
+class HomeView extends ConsumerStatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  ConsumerState<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<HomeView> {
+  late final PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+
+    _pageController.addListener(() {
+      final notifier = ref.read(pokemonProvider.notifier);
+      final currentPage = _pageController.page;
+
+      final totalPages = ref.read(pokemonProvider).maybeWhen(
+            data: (data) => data.pokemons.length,
+            orElse: () => 0,
+          );
+
+      if (currentPage != null && currentPage >= totalPages - 2) {
+        notifier.fetchMorePokemons();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final pokemon = ref.watch(pokemonProvider);
+
+    return pokemon.when(
+      data: (data) {
+        return PageView.builder(
+          controller: _pageController,
+          scrollDirection: Axis.vertical,
+          itemCount: data.pokemons.length,
+          itemBuilder: (context, index) => PokemonMainView(pokemon: data.pokemons[index]),
+        );
+      },
+      error: (error, stackTrace) => Center(
+        child: Text(
+          error.toString(),
+        ),
+      ),
+      loading: () => Center(
+        child: const CircularProgressIndicator(),
+      ),
+    );
+  }
+}
